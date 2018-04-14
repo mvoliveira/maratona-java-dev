01d769cc0ac558a3937e91d2443019feeca1fadc
Matheus <matheus.mvo10@gmail.com>
2018-04-14 09:48:28 -0300

    Add basic Operator

---
+++ src/Operators.java
@@ -0,0 +1,9 @@
+public class Operators {
+    public static void main(String[] args) {
+        int firstNumber = 10;
+        int secondNumber = 20;
+        int result = firstNumber + secondNumber;
+        System.out.println(result);
+    }
+}
+
