eb0baba5bfdfde813a4b30ae756a67c473dcea23
Matheus <matheus.mvo10@gmail.com>
2018-04-14 13:12:20 -0300

    Refactor Part of Paymente no bigger than 1000

--- src/PartOfPayment.java
+++ src/PartOfPayment.java
@@ -2,14 +2,12 @@ public class PartOfPayment {
     public static void main(String[] args) {
         double value = 30000;
 
-
         for (int partOfPayment = 1; partOfPayment < value; partOfPayment++) {
             double valuePart = value / partOfPayment;
-            if (valuePart >= 1000) {
-                System.out.println(valuePart);
-                continue;
+            if (valuePart < 1000) {
+                break;
             }
-            break;
+            System.out.println(valuePart);
         }
     }
 }
