e4f158bc17cc9a6eaceec224244202ee220d9756
Matheus <matheus.mvo10@gmail.com>
2018-04-14 09:50:59 -0300

    Add mod

--- src/Operators.java
+++ src/Operators.java
@@ -2,7 +2,11 @@ public class Operators {
     public static void main(String[] args) {
         int firstNumber = 10;
         int secondNumber = 20;
-        int result = firstNumber + secondNumber;
+//        int result = firstNumber + secondNumber;
+//        int result = firstNumber - secondNumber;
+//        int result = firstNumber / secondNumber;
+//        int result = firstNumber * secondNumber;
+        int result = firstNumber % secondNumber;
         System.out.println(result);
     }
 }
