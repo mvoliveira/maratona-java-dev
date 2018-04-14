d80f96743b7fea5951b57f6a0aaa0b8bfb64553e
Matheus <matheus.mvo10@gmail.com>
2018-04-14 11:12:55 -0300

    Salary Tax calculator

---
+++ src/SalaryTax.java
@@ -0,0 +1,16 @@
+public class SalaryTax {
+    public static void main(String[] args) {
+        double salary = 5001;
+        double totalTax = 0;
+    if (salary < 1000) {
+        totalTax = salary * 5/100;
+    } else if (salary >= 1000 && salary < 2000){
+        totalTax = salary * 10/100;
+    } else if (salary >= 2000 && salary < 4000){
+        totalTax = salary * 15/100;
+    } else if (salary > 5000){
+        totalTax = salary * 20/100;
+    }
+        System.out.println(totalTax);
+    }
+}
