8547d27fde0c5caaa710524fcb56a8090c39246e
Matheus <matheus.mvo10@gmail.com>
2018-04-14 17:12:29 -0300

    Remove spaces

--- src/MultidimensionalArrayJava.java
+++ src/MultidimensionalArrayJava.java
@@ -6,24 +6,19 @@ public class MultidimensionalArrayJava {
         multidimensionalArrayJava[1][0] = 2;
         multidimensionalArrayJava[0][1] = 3;
         multidimensionalArrayJava[1][1] = 4;
-
 //        print hardcode
         System.out.println(multidimensionalArrayJava[1][1]);
-
 //        print using for
         for (int i = 0; i < multidimensionalArrayJava.length; i++) {
             for (int j = 0; j < multidimensionalArrayJava[i].length; j++)
                 System.out.println(multidimensionalArrayJava[i][j]);
         }
-
 //        print using for each
         for (int[] ref : multidimensionalArrayJava) {
             for (int numbers : ref) {
                 System.out.println(numbers);
             }
-
         }
-
     }
 }
 
