52a12e3e652fb49497f47bda0872ab6253ddbdfb
Matheus <matheus.mvo10@gmail.com>
2018-04-14 13:55:07 -0300

    Empty Array Int

---
+++ src/EmptyArray.java
@@ -0,0 +1,10 @@
+public class EmptyArray {
+    public static void main(String[] args) {
+        int[] emptyArrayInts = new int[3];
+
+        System.out.println(emptyArrayInts[0]);
+        System.out.println(emptyArrayInts[1]);
+        System.out.println(emptyArrayInts[2]);
+    }
+}
+
