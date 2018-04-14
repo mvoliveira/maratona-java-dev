4f5d76ed511218b6580752f8bdc8c5a32062f363
Matheus <matheus.mvo10@gmail.com>
2018-04-14 13:46:11 -0300

    New Array

---
+++ src/NewArray.java
@@ -0,0 +1,12 @@
+public class NewArray {
+    public static void main(String[] args) {
+        int[] array = new int[3];
+        array[0] = 1;
+        array[1] = 2;
+        array[2] = 3;
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+    }
+}
