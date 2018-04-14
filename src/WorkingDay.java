7a7731d97f7231e9b13de1f1ce6be2ed108a7894
Matheus <matheus.mvo10@gmail.com>
2018-04-14 11:35:13 -0300

    Working days Switch

--- src/WorkingDay.java
+++ src/WorkingDay.java
@@ -1,3 +1,20 @@
 public class WorkingDay {
-
+    public static void main(String[] args) {
+        byte day = 3 ;
+        switch (day) {
+            case 1:
+            case 7:
+                System.out.println("final de semana");
+                break;
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+            case 6:
+                System.out.println("dia útil");
+                break;
+            default:
+                System.out.println("opção inválida");
+        }
+    }
 }
