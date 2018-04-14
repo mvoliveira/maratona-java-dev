0d60a9e5375aed55e700f1d7c6dfcb2fc4fa2f64
Matheus <matheus.mvo10@gmail.com>
2018-04-14 15:11:48 -0300

    Multidimensional Array Java with for each

--- src/MultidimensionalArrayJava.java
+++ src/MultidimensionalArrayJava.java
@@ -4,16 +4,22 @@ public class MultidimensionalArrayJava {
 
         multidimensionalArrayJava[0][0] = 1;
         multidimensionalArrayJava[1][0] = 2;
-        multidimensionalArrayJava[0][1] = 2;
-        multidimensionalArrayJava[1][1] = 3;
+        multidimensionalArrayJava[0][1] = 3;
+        multidimensionalArrayJava[1][1] = 4;
 
-        System.out.println(multidimensionalArrayJava[1][1]);
+//        System.out.println(multidimensionalArrayJava[1][1]);
+//
+//        for (int i = 0; i < multidimensionalArrayJava.length; i++) {
+//            for (int j = 0; j < multidimensionalArrayJava[i].length; j++)
+//            System.out.println(multidimensionalArrayJava[i][j]);
+//        }
 
-        for (int i = 0; i < multidimensionalArrayJava.length; i++) {
-            for (int j = 0; j < multidimensionalArrayJava[i].length; j++)
-            System.out.println(multidimensionalArrayJava[i][j]);
+        for (int[] ref : multidimensionalArrayJava) {
+            for (int numbers : ref){
+                System.out.println(numbers);
+            }
+
         }
-
 
     }
 }
