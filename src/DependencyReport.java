1d444a37aa68b5c8b2d059831a75f337fd0c1aeb
Matheus <matheus.mvo10@gmail.com>
2018-04-14 09:32:57 -0300

    Adiciona Dependency Report

---
+++ src/DependencyReport.java
@@ -0,0 +1,8 @@
+public class DependencyReport {
+    public static void main(String[] args) {
+        int phoneNumber = 34804931;
+        String name = "Matheus";
+        String address = "Av Osvaldo Aranha";
+        System.out.println("O domiciliado: " + name + " no endereço " + address + " e telefone " + phoneNumber + " não possui nenhum tipo de dependencia.");
+    }
+}
