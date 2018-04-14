f3c154ebdceaba0d0ca863b8cabdb70bdeceb3b1
Matheus <matheus.mvo10@gmail.com>
2018-04-14 20:35:21 -0300

    Valida divisão por 0

--- src/intromethods/classes/Calculadora.java
+++ src/intromethods/classes/Calculadora.java
@@ -1,19 +1,22 @@
 package intromethods.classes;
 
 public class Calculadora {
-    public void addTwoNumbers(){
-        System.out.println(5+5);
+    public void addTwoNumbers() {
+        System.out.println(5 + 5);
     }
 
-    public void subtractTwoNumbers(){
-        System.out.println(5-5);
+    public void subtractTwoNumbers() {
+        System.out.println(5 - 5);
     }
 
-    public void multiplyTwoNumbers(int firstNumber, int secondNumber){
-        System.out.println(firstNumber*secondNumber);
+    public void multiplyTwoNumbers(int firstNumber, int secondNumber) {
+        System.out.println(firstNumber * secondNumber);
     }
 
-    public double divideTwoNumbers(int firtsNumber, int secondNumber){
-        return firtsNumber / secondNumber;
+    public double divideTwoNumbers(int firtsNumber, int secondNumber) {
+        if (secondNumber != 0) {
+            return firtsNumber / secondNumber;
+        }
+        return 0;
     }
 }
