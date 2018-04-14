e612438306e5ab48bf4f145985f886113f01dd87
Matheus <matheus.mvo10@gmail.com>
2018-04-14 14:00:45 -0300

    Empty Array Int

--- src/EmptyArray.java
+++ src/EmptyArray.java
@@ -2,6 +2,7 @@ public class EmptyArray {
     public static void main(String[] args) {
         int[] emptyArrayInts = new int[3];
 
+//        is the same resulto for byte, short long,float, doable;
         System.out.println(emptyArrayInts[0]);
         System.out.println(emptyArrayInts[1]);
         System.out.println(emptyArrayInts[2]);
@@ -11,6 +12,7 @@ public class EmptyArray {
         System.out.println(emptyArrayString[0]);
         System.out.println(emptyArrayString[1]);
         System.out.println(emptyArrayString[2]);
+
     }
 }
 
