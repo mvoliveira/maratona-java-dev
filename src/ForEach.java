01d5b1e200c505f21179d09a0b4380e286e0f836
Matheus <matheus.mvo10@gmail.com>
2018-04-14 14:20:06 -0300

    For each syntax

---
+++ src/ForEach.java
@@ -0,0 +1,9 @@
+public class ForEach {
+    public static void main(String[] args) {
+
+        String[] jogadores = {"Jael", "Geromel", "Marcelo"};
+        for (String jogador : jogadores) {
+            System.out.println (jogador);
+        }
+    }
+}
