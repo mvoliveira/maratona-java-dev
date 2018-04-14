807a8d30ddb7f8bbd0108ccc2f0941d6339f7667
Matheus <matheus.mvo10@gmail.com>
2018-04-14 10:41:41 -0300

    Refactor with ternary operator

---
+++ src/TernaryOperator.java
@@ -0,0 +1,10 @@
+public class TernaryOperator {
+    public static void main(String[] args) {
+        int age = 18;
+        String status;
+
+        status = age >= 18 ? "adulto" : "Não adulto";
+        System.out.println(status);
+
+    }
+}
