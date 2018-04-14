2f00090a7bf0581128c6b04341c1d9350881726c
Matheus <matheus.mvo10@gmail.com>
2018-04-14 09:41:51 -0300

    Add Employee

---
+++ src/Employee.java
@@ -0,0 +1,9 @@
+public class Employee {
+    public static void main(String[] args) {
+        String name = "José";
+        String sexo = "masculino";
+        int salary = 3670;
+        String maritalStatus = "solteiro";
+        System.out.println("O trabalhador " + name + " do sexo " + sexo + " salario " + salary + " e estado civil " + maritalStatus + " encontra-se empregado aqui.");
+    }
+}
