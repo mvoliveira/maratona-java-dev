28d63ad3360db7fc915844302cae2cbc00e6cb1d
Matheus <matheus.mvo10@gmail.com>
2018-04-15 22:29:21 -0300

    Adiciona chamado de método nNumbers

--- src/javacore/intromethods/test/CalculadoraTest.java
+++ src/javacore/intromethods/test/CalculadoraTest.java
@@ -9,5 +9,8 @@ public class CalculadoraTest {
         calculadora.subtractTwoNumbers();
         calculadora.multiplyTwoNumbers(5,5);
         System.out.println(calculadora.divideTwoNumbers(20,0));
+
+        int nNumbers[] = {14,12,23,4};
+        calculadora.addNNumbers(nNumbers);
     }
 }
