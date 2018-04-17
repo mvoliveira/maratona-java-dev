9337d9895200c11f2d8fee68d74db936e068c911
Matheus <matheus.mvo10@gmail.com>
2018-04-17 15:39:38 -0300

    Adiciona validação assessments diferente de null

--- src/javacore/intromethods/classes/Student.java
+++ src/javacore/intromethods/classes/Student.java
@@ -7,24 +7,31 @@ public class Student {
     public double[] assessments;
 
     public void printStudent() {
-        System.out.println(this.name);
-        System.out.println(this.age);
-        for (double assessment : assessments) {
-            System.out.print(assessment + ", ");
+        System.out.println("Nome: " + this.name);
+        System.out.println("Idade: " + this.age);
+        if (assessments != null) {
+            for (double assessment : assessments) {
+                System.out.println(assessment + ", ");
+            }
         }
     }
 
     public void average() {
+        if (assessments == null) {
+            System.out.println("Este aluno não possui notas");
+            System.out.println("O aluno foi reprovado");
+            return;
+        }
         double average = 0;
         for (double assessment : assessments) {
             average += assessment;
         }
         average = average / assessments.length;
-        System.out.println("Média: " + average);
-        if (average > 6){
-            System.out.println("aprovado");
-        } else{
-            System.out.println("reprovado");
+        System.out.println("A média é: " + average);
+        if (average > 6) {
+            System.out.println("O aluno foi aprovado");
+        } else {
+            System.out.println("O aluno foi reprovado");
         }
     }
 }
