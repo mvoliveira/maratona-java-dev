6149b8dca30ddb262727ddc91c8e1f34d979cc8c
Matheus <matheus.mvo10@gmail.com>
2018-04-17 15:55:44 -0300

    Encapsula atributos da classe Student

--- src/javacore/intromethods/classes/Student.java
+++ src/javacore/intromethods/classes/Student.java
@@ -2,36 +2,48 @@ package javacore.intromethods.classes;
 
 public class Student {
 
-    public String name;
-    public int age;
-    public double[] assessments;
+    private String name;
+    private int age;
+    private double[] assessments;
 
     public void printStudent() {
         System.out.println("Nome: " + this.name);
         System.out.println("Idade: " + this.age);
-        if (assessments != null) {
-            for (double assessment : assessments) {
+        if (this.assessments != null) {
+            for (double assessment : this.assessments) {
                 System.out.println(assessment + ", ");
             }
         }
     }
 
     public void average() {
-        if (assessments == null) {
+        if (this.assessments == null) {
             System.out.println("Este aluno não possui notas");
             System.out.println("O aluno foi reprovado");
             return;
         }
         double average = 0;
-        for (double assessment : assessments) {
+        for (double assessment : this.assessments) {
             average += assessment;
         }
-        average = average / assessments.length;
+        average = average / this.assessments.length;
         System.out.println("A média é: " + average);
         if (average > 6) {
             System.out.println("O aluno foi aprovado");
         } else {
             System.out.println("O aluno foi reprovado");
         }
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void setAge(int age){
+        this.age = age;
+    }
+
+    public void setAssessments(double[] assessments){
+        this.assessments = assessments;
     }
 }
