11d448b2eec1094ff8a9c810fb8da45ee40c8db8
Matheus <matheus.mvo10@gmail.com>
2018-04-17 19:38:19 -0300

    Adiciona chamada de função do métod isApproved

--- src/javacore/intromethods/test/StudentTest.java
+++ src/javacore/intromethods/test/StudentTest.java
@@ -6,11 +6,13 @@ public class StudentTest {
     public static void main(String[] args) {
         Student jairo = new Student();
         jairo.setName("Jairão");
-        jairo.setAge(18);
-        jairo.setAssessments(new double[]{8,9,9});
+        jairo.setAge(41);
+        jairo.setAssessments(new double[]{8, 1, 1});
+
 
         jairo.printStudent();
         jairo.average();
+        System.out.println("Estudante aprovado?" + jairo.isApproved());
 
     }
 }
