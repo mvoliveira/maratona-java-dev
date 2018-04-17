2f503ed355a6189ae92bc1efd519eed900e4bc1e
Matheus <matheus.mvo10@gmail.com>
2018-04-17 15:56:11 -0300

    Encapsula atributos da classe Student

--- src/javacore/intromethods/test/StudentTest.java
+++ src/javacore/intromethods/test/StudentTest.java
@@ -5,9 +5,9 @@ import javacore.intromethods.classes.Student;
 public class StudentTest {
     public static void main(String[] args) {
         Student jairo = new Student();
-        jairo.name = "jairooo";
-        jairo.age = 18;
-        jairo.assessments = new double[]{9, 7, 4};
+        jairo.setName("Jairão");
+        jairo.setAge(18);
+        jairo.setAssessments(new double[]{8,9,9});
 
         jairo.printStudent();
         jairo.average();
