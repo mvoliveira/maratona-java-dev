72e6a9ba6e6dcff6d014e626aed27266531065ff
Matheus <matheus.mvo10@gmail.com>
2018-04-17 19:44:44 -0300

    Adiciona classe Employee

---
+++ src/javacore/overload/classes/Employee.java
@@ -0,0 +1,38 @@
+package javacore.overload.classes;
+
+public class Employee {
+    private String name;
+    private int age;
+    private double salary;
+
+    public void init(String name, int age, double salary){
+        this.name = name;
+        this.age= age;
+        this.salary = salary;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+}
